54ae1fcf73011c58cf66562a5845d1ae4fa1237b
SorPuti <lucashdk2@gmail.com>
2022-03-04 16:07:58 -0300

    ZikBOt biblioteca extensiva

---
+++ src/br/com/githu/zik/client/api/Calculate.java
@@ -0,0 +1,9 @@
+package br.com.githu.zik.client.api;
+
+public class Calculate {
+	
+	public static double discount(double valor) {
+		return (valor * 9) / 30;
+	}
+
+}
---
+++ src/br/com/githu/zik/client/api/Main.java
@@ -0,0 +1,8 @@
+package br.com.githu.zik.client.api;
+
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
